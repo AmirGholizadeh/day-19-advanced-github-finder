5dd001326c373091509586b9c639e2b0ab6009f0
Amir <amir110ghoolizadeh@gmail.com>
2019-08-12 14:18:16 +0430

    create Add component

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-class Child extends React.Component{
-    render(){
-        return <h1 onMouseEnter={this.props.onMouseEnter}>Hello {this.props.name}, it seems that you are {this.props.age} years old.(you clicked this sentence for {this.props.points}</h1>
+
+class Find{
+    constructor(){
+        this.clientId='6c08bd510d2109b3d14e';
+        this.clientSecret="29a2a5c2f12ad27490e5ea5dff32925dedf893b4";
     }
+    
 }
-class Parent extends React.Component{
+ class Add extends React.Component{
     constructor(props){
         super(props);
-        this.state={name : 'A',age:16,points:0};
-        this.handleMouseEnter = this.handleMouseEnter.bind(this);
-    }
-    handleMouseEnter(){
-        const total = this.state.points;
-        this.setState({points : total + 1});
     }
     render(){
-        return (
-            <div>
-                <Child name={this.state.name}  age={this.state.age} onMouseEnter={this.handleMouseEnter} points={this.state.points}/>
+        return(
+            <div className="output">
+                <div className="box-1">
+                    <h1 id="name">{this.props.name}</h1>
+                    <a href={this.props.url}><img src={this.props.avatar}/></a>
+                    </div>
+                <ul className="box-2">
+                        <li id="login">Logined as : {this.props.login}</li>
+                        <li id="id">ID : {this.props.id}</li>
+                        <li id="followers">{this.props.followersNumber} Followers in total, Including {this.props.followers}</li>
+                        <li id="following">{this.props.following} Following in total</li>
+                        <li id="type">Type : {this.props.type}</li>
+                        <li id="company">Company : {this.props.company}</li>
+                        <li id="location">Location : {this.props.location}</li>
+                        <li id="email">Email : {this.props.email}</li>
+                        <li id="bio">Bio : {this.props.bio}</li>
+                        <li id="hireable">Hireable : {this.props.hire}</li>
+                        <li id="public-repos">Public repos : {this.props.pRepos}</li>
+                        <li id="public-gists">Public gists : {this.props.pGists}</li>
+                        <li id="created-at">Created at : {this.props.created}</li>
+                        <li id="updated-at">Last updated at : {this.props.updated}</li>
+                        <li id="repos">Repos : {this.props.repos}</li>
+                </ul>
             </div>
-        )
+        );
     }
 }
-ReactDOM.render(<Parent />, document.getElementById('root'));
