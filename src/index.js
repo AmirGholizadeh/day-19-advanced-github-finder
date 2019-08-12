0f0f54772263e3e1efbdd95e0162df23fdb858e7
Amir <amir110ghoolizadeh@gmail.com>
2019-08-12 12:28:06 +0430

    starting point

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Child extends React.Component{
+    render(){
+        return <h1 onMouseEnter={this.props.onMouseEnter}>Hello {this.props.name}, it seems that you are {this.props.age} years old.(you clicked this sentence for {this.props.points}</h1>
+    }
+}
+class Parent extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={name : 'A',age:16,points:0};
+        this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    }
+    handleMouseEnter(){
+        const total = this.state.points;
+        this.setState({points : total + 1});
+    }
+    render(){
+        return (
+            <div>
+                <Child name={this.state.name}  age={this.state.age} onMouseEnter={this.handleMouseEnter} points={this.state.points}/>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<Parent />, document.getElementById('root'));
